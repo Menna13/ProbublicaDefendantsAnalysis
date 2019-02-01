0dd03e913a748698911baf104788f97a75a94d5d
Sorelle Friedler <sorelle@cs.haverford.edu>
2019-01-31 22:12:33 -0500

    initial commit of stub files

---
+++ src/main/java/propublica/datadesign/Main.java
@@ -0,0 +1,13 @@
+package propublica.datadesign;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
