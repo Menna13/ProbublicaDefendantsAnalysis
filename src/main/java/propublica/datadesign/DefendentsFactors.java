189add0dc4cff0626e858b34d23d9f907a68dca7
mkhaliel <mkhaliel@blum>
2019-02-12 04:24:01 -0500

    .

--- src/main/java/propublica/datadesign/DefendentsFactors.java
+++ src/main/java/propublica/datadesign/DefendentsFactors.java
@@ -1,118 +1,194 @@
+
+/* Name: Menna Khaliel
+ * File: DefendentsFactors.java
+ * 
+ * The main class for Lab 1 
+ * 
+ * This program creates one row based on the data from 
+ * compas-scores.csv that's obtained from Probublica's analysis.
+ * It makes a simple analysis based on two factors from the data 
+ * which are score of defendants and two-years redcideivsm 
+ * It checks whether the inputs of each criteria in the row
+ * is a valid input or not based on an analysis of the given data in the file. 
+ */
+
 package propublica.datadesign;
-
-
-
 
 public class DefendentsFactors {
 
+	enum Sex {MALE , FEMALE};
+	enum Race {AFRICANAMERICAN, OTHER, HISPANIC, CAUCASIAN};
+	enum ScoreText {LOW, HIGH, MEDIUM};
+	enum CChargeDegree {F, M}; 
 
-	private String Sex;
-	private String Race;
-	private	String cchargeDegree;
+	//THINGS LIKE MALE SHOULD BE IN CAPS (ENUM PARAMETERS)
+
+
+
+	// initializing the ten fields that exist in the row
+	private Sex sex;
+	private Race race;
+	private	CChargeDegree cchargeDegree;
 	private	String rchargeDesc;
 	private	int decileScore;
-	private	String scoreTest;
+	private	ScoreText scoreText;
 	private	int twoyearrecid;
 	private String cchargeDesc;
 	private	String rchargeDegree;
 	private	String sexRace;
 
 
-	public DefendentsFactors (String Sex, String Race,String cchargeDegree, String rchargeDesc, int decileScore, String scoreTest, 
+	//setting the constructor
+	public DefendentsFactors (String Sex, String Race,CChargeDegree cchargeDegree, 
+			String rchargeDesc, int decileScore, ScoreText scoreText, 
 			int twoyearrecid, String cchargeDesc, String rchargeDegree, String sexRace) {
 
-
-		this.Sex = Sex;
-		this.Race = Race;
+		this.sex = sex;
+		this.race = race;
 		this.cchargeDegree = cchargeDegree;
 		this.rchargeDesc = rchargeDesc;
 		this.decileScore = decileScore;
-		this.scoreTest = scoreTest;
+		this.scoreText = scoreText;
 		this.twoyearrecid = twoyearrecid;
 		this.cchargeDesc = cchargeDesc;
 		this.rchargeDegree = rchargeDegree;
 		this.sexRace = sexRace;
 	}
 
-	private String getSex() {
-		return this.Sex;
+
+	//Setting Getters and Setters
+
+	public Sex getSex() {
+		return sex;
 	}
 
-	private String getRace() {
-		return this.Race;
+
+
+	public void setSex(Sex sex) {
+		this.sex = sex;
 	}
 
-	private String getcchargeDesc() {
-		return this.cchargeDesc;
+
+
+	public Race getRace() {
+		return race;
 	}
 
-	private int gettwoyearrecid() {
-		return this.twoyearrecid; 
+
+
+	public void setRace(Race race) {
+		this.race = race;
 	}
 
-	private int getdecileScore() {
-		return this.decileScore;
+
+
+	public CChargeDegree getCchargeDegree() {
+		return cchargeDegree;
 	}
 
-	private String getcchargeDegree () {
-		return this.cchargeDegree;
+
+
+	public void setCchargeDegree(CChargeDegree cchargeDegree) {
+		this.cchargeDegree = cchargeDegree;
 	}
 
-	private void setSex (String sex) {
 
-		this.Sex = sex; 
+
+	public String getRchargeDesc() {
+		return rchargeDesc;
 	}
 
-	private void setRace (String race) {
 
-		this.Race = race; 
+
+	public void setRchargeDesc(String rchargeDesc) {
+		this.rchargeDesc = rchargeDesc;
 	}
 
-	private void cchargeDegree(String cchargedegree) {
 
-		this.cchargeDegree = cchargedegree; 
 
+	public int getDecileScore() {
+		return decileScore;
 	}
 
-	private void rchargeDesc (String rchargedesc) {
 
-		this.rchargeDesc = rchargedesc; 
 
+	public void setDecileScore(int decileScore) {
+		this.decileScore = decileScore;
 	}
 
-	private void decileScore (int decilescore) {
 
-		this.decileScore = decilescore; 
+
+	public ScoreText getScoreText() {
+		return scoreText;
 	}
 
-	private void scoreTest (String scoretest) {
 
-		this.scoreTest = scoretest; 
+
+	public void setScoreText(ScoreText scoreText) {
+		this.scoreText = scoreText;
 	}
 
-	private void twoyearrecid (int twoyearRecid) {
 
-		this.twoyearrecid = twoyearRecid; 
+
+	public int getTwoyearrecid() {
+		return twoyearrecid;
 	}
 
-	private void cchargeDesc (String cchargedesc) {
 
-		this.cchargeDesc = cchargedesc; 
+
+	public void setTwoyearrecid(int twoyearrecid) {
+		this.twoyearrecid = twoyearrecid;
 	}
 
-	private void rchargeDegree (String rchargedegree) {
 
-		this.rchargeDegree = rchargedegree; 
+
+	public String getCchargeDesc() {
+		return cchargeDesc;
 	}
 
-	private void sexRace(String sexrace) {
 
-		this.sexRace = sexrace; 
+
+	public void setCchargeDesc(String cchargeDesc) {
+		this.cchargeDesc = cchargeDesc;
 	}
+
+
+
+	public String getRchargeDegree() {
+		return rchargeDegree;
+	}
+
+
+
+	public void setRchargeDegree(String rchargeDegree) {
+		this.rchargeDegree = rchargeDegree;
+	}
+
+
+
+	public String getSexRace() {
+		return sexRace;
+	}
+
+
+
+	public void setSexRace(String sexRace) {
+		this.sexRace = sexRace;
+	}
+
+	/*
+	 *returns true or false based on determining if the machine is
+	 *biased and creates High Risk. It compares the the score test 
+	 *if it's high with no recid in two years, then it's biased
+	 *@param1 scoreText is the the score
+	 *@param2 twoyearrecid is an input of 1 or 0 which means that there 
+	 *was a recid in case of 1 and not in case of 0
+	 *return boolean true or false based on the analysis 
+	 */
 
 	private boolean ScoreAnalysisBiasedToHigh () {
 
-		if (this.scoreTest == "High" && this.twoyearrecid == 0) {
+		if (this.scoreText == ScoreText.HIGH && this.twoyearrecid == 0) {
 			return true;
 		}
 
@@ -120,38 +196,188 @@ public class DefendentsFactors {
 			return false;
 		}
 	}
+
+
+	/*
+	 *returns true or false based on determining if the machine is
+	 *biased and creates Low Risk. It compares the the score test 
+	 *if it's high with no recid in two years, then it's biased
+	 *@param1 scoreText is the the score
+	 *@param2 twoyearrecid is an input of 1 or 0 which means that there 
+	 *was a recid in case of 1 and not in case of 0
+	 *return boolean true or false based on the analysis 
+	 */
+
 	private boolean ScoreAnalysisBiasedToLow () {
 
-		if (this.scoreTest == "Low" && this.twoyearrecid == 1) {
+		if (this.scoreText == ScoreText.LOW && this.twoyearrecid == 1) { 
 			return true;
 		}
-
 		else {
 			return false;
 		}
 	}
 
-
-	public DefendentsFactors (String Sex, String Race,String cchargeDegree, String rchargeDesc, String decileScore, String scoreTest, 
+	//setting a constructor that takes only string inputs
+	public DefendentsFactors (String Sex, String Race, String CChargeDegree, 
+			String rchargeDesc, String decileScore, String ScoreText, 
 			String twoyearrecid, String cchargeDesc, String rchargeDegree, String sexRace) {
 
 
 		this.decileScore = Integer.parseInt(decileScore);
 		this.twoyearrecid = Integer.parseInt(twoyearrecid);
-		this.Sex = Sex;
-		this.Race = Race;
+		this.sex = sex;
+		this.race = race;
 		this.cchargeDegree = cchargeDegree;
-		this.rchargeDesc = rchargeDesc;
-		this.scoreTest = scoreTest;
-		this.cchargeDesc = cchargeDesc;
-		this.rchargeDegree = rchargeDegree;
-		this.sexRace = sexRace;
-		
-		
+		this.scoreText = scoreText;
 	}
-	
 
+	/*
+	 * return the decile score as a String and throws
+	 * and exception if it's not valid (if it's less
+	 *  than 0 or greater than 10) based on the data 
+	 *  from csv file
+	 *  @param decileScore the integer input to convert to String
+	 *  returns an exception if the input is invalid 
+	 *  and string score number if it's valid
+	 */
+
+	private void ConvertDecile (String decileScore){
+		//creating a new variable and converting the integer decileScore into string 
+		int initialdecileScore = Integer.parseInt(decileScore); 
+		//testing whether it's valid input or not
+		if (initialdecileScore <= 10 && initialdecileScore > 0) { 
+			//assigning the number converted into string to decileScore
+			this.decileScore = Integer.parseInt(decileScore); 
+		}	else { 
+			//throws an exception if it's not
+			throw new IllegalArgumentException("Invalid Input"); 
+		}
+	}
+
+
+	/*
+	 * return the twoyearrecid as a string and throws
+	 * an exception if it's not valid (if it's anything 
+	 * other than 0 and 1 based on the data from the csv file
+	 * @param twoyearrecid the integer input to convert to String
+	 * returns an exception if the input is invalid
+	 * and string number (either 1 or 0) if it's valid
+	 */
+
+	private void Converttwoyearrecid (String twoyearrecid){
+		//creating a new variable and converting the integer twoyearrecid into string
+		int initialtwoyearrecid = Integer.parseInt(twoyearrecid);
+		//testing whether it's valid input or not
+		if (initialtwoyearrecid == 0) {
+			//assigning the number converted into string to twoyearrecid
+			this.twoyearrecid = Integer.parseInt(twoyearrecid); 
+		}
+		else if (initialtwoyearrecid == 1) {
+			//assigning the number converted into string to twoyearrecid
+			this.twoyearrecid = Integer.parseInt(twoyearrecid);
+		}else {
+			//throws an exception if it's not
+			throw new IllegalArgumentException("Invalid Input"); 
+		}
+	}
+
+	/*
+	 * ensures that this field holds valid inputs 
+	 * (Male and Female) based on the data from the csv file
+	 * @param sex is either Male or Female 
+	 * returns an exception if the input is invalid
+	 * and calls for MALE or FEMALE from enum Sex if it's valid
+	 */
+
+	private void CheckSex (String sex) {
+		if (sex == "Male") {
+			//calls for the matching input from enum Sex
+			this.sex = Sex.MALE; 
+		}else if (sex == "Female") {
+			//calls for the matching input from enum Sex
+			this.sex = Sex.FEMALE;
+		}else {
+			//throws an exception if it's not valid
+			throw new IllegalArgumentException("Invalid Input");  
+		}
+	}
+
+	/*
+	 * ensures that this field holds valid inputs
+	 * (African-American, Hispanic, Caucasian, Other)
+	 * @param race is one of the five specified inputs
+	 * returns an exception if the input is invalid
+	 * and calls for the inputs from enum Race if it's valid
+	 */
+
+	private void Checkrace(String race){
+		if (race == "African-American") {
+			//calls for the matching input from enum Race
+			this.race = Race.AFRICANAMERICAN;
+		} else if (race == "Hispanic") {
+			this.race = Race.HISPANIC;
+		} else if (race == "Caucasian") {
+			this.race = Race.CAUCASIAN;
+		} else if (race == "Other") {
+			this.race = Race.OTHER;		
+		}
+		else {
+			//throws an exception if it's not valid
+			throw new IllegalArgumentException("Invalid Input");
+		}
+	}
+
+	/*
+	 * ensures that this field holds valid inputs (F, M)
+	 * @param cchargeDegree is either F or M
+	 * returns an exception if the input is invalid
+	 * and calls for the inputs from enum CheckCChargeDegree if it's valid
+	 */
+
+	private void CheckCChargeDegree (String cchargeDegree) {
+		if (cchargeDegree == "M") {
+			//calls for the matching input from enum CheckCChargeDegree
+			this.cchargeDegree = CChargeDegree.M;
+		}else if (cchargeDegree == "F") {
+			this.cchargeDegree = CChargeDegree.F;
+		}else {
+			//throws an exception if it's not valid
+			throw new IllegalArgumentException("Invalid Input");
+		}
+	}
+
+	/*
+	 * ensures that this field holds valid inputs
+	 * (High, Medium, Low)
+	 * @param scoreText is to be one of the three 
+	 * specified inputs
+	 * returns an exception if the input is invalid
+	 * and calls for either on of the inputs 
+	 * from enum CheckScoreText if it's valid
+	 */
+
+	private void CheckScoreText (String scoreText) {
+		if (scoreText == "Low") {
+			//calls for the matching input from enum ScoreText
+			this.scoreText = ScoreText.LOW;
+		}else if (scoreText == "Medium") {
+			this.scoreText = ScoreText.MEDIUM;
+		}else if (scoreText == "High") {
+			this.scoreText = ScoreText.HIGH;
+		}else {
+			//throws an exception if it's not valid
+			throw new IllegalArgumentException("Invalid Input");
+		}
+	}
 }
+
+
+
+
+
+
+
 
 
 
--- src/main/java/propublica/datadesign/Main.java
+++ src/main/java/propublica/datadesign/Main.java
@@ -1,15 +1,25 @@
 package propublica.datadesign;
 
-/**
- * Hello world!
- *
+/* Name: Menna Khaliel
+ * File: Main.java
+ * 
+ * The main method that tests the rows created by the the class and methods in the DefendentsFactors file
+
  */
+
 public class Main 
 {
 	public static void main( String[] args )
 	{
-	
+
+		// valid input for the row
+		DefendentsFactors(Sex.MALE, Race.AFRICANAMERICAN, CChargeDegree.M, 
+				"Driving License Suspended", 6 , ScoreText.Medium, false , "Battery", "(F2)", "Male-African-American");
+		
+		//has invalid input for the row (descile score can't be higer than 10)
+		DefendentsFactors("Male", "African-American","M", 
+				"Driving License Suspended", "13", "Medium", "false", "Battery", "(M2)", "Male-African-American"));
+
+
 	}
-
-
-			
+}
